709a83f68bd678d375ccaba5f10bec2790bf4c18
Tobiasz Jędrzejek <tobiasz.jedrzejek@gmail.com>
2022-10-16 20:02:35 +0200

    Updating App.java

--- zadanie7/src/main/java/pt/trainings/maven/list1/ex7/App.java
+++ zadanie7/src/main/java/pt/trainings/maven/list1/ex7/App.java
@@ -5,10 +5,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "I am going to calculate 2 * 2\n" );
-        int x,y;
+        int x;
+        int y;
         x = 2;
         y = 2;
-        Calculate calc = new Calculate();
+        final Calculate calc = new Calculate();
         calc.multiply(x, y);
     }
 }
